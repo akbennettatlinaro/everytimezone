68deac86163b2f117c54f681b9f05b1725b9d926
Alan Bennett <alan.bennett@linaro.org>
2017-03-23 12:26:16 -0600

    Initial files from http://everytimezone.com
    Signed-off-by: Alan Bennett <alan.bennett@linaro.org>

---
+++ Time Zones_files/in.php
@@ -0,0 +1,3 @@
+// static32
+var NO_PINGY=1;(function(){var d=new Date;d.setTime(d.getTime()+3600*1000);document.cookie="no_trackyy_204109=1;expires="+d.toGMTString()+";path=/;";})();
+// exit bad site id
