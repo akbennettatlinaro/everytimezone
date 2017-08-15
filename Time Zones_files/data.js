c03af69155005294b9689a68cbf4091f915454f1
Alan Bennett <alan.bennett@linaro.org>
2017-08-14 21:38:18 -0600

    add Tokyo

--- Time Zones_files/data.js
+++ Time Zones_files/data.js
@@ -1,1 +1,1 @@
-var data = [[-7.0,"Mike, Tyler","PDT UTC-7",true],[-6.0,"Alan","MDT UTC-6",true],[-5.0,"Andy","CDT UTC-5",true],[-4.0,"Marti","EDT UTC-4",true],[-3.0,"Ricardo","BRT UTC-3",true],[0.0,"George","GMT UTC",true],[1.0,"Milo","CET UTC+1",true]];
+var data = [[-7.0,"Mike, Tyler","PDT UTC-7",true],[-6.0,"Alan","MDT UTC-6",true],[-5.0,"Andy","CDT UTC-5",true],[-4.0,"Marti","EDT UTC-4",true],[-3.0,"Ricardo","BRT UTC-3",true],[0.0,"George","GMT UTC",true],[1.0,"Milo","CET UTC+1",true],[9.0,"Tokyo","JST UTC+9",true]];
