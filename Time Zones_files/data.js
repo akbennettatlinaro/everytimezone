b33e17f7f695ea64ecf995a58f1d0a1e4520133e
Alan Bennett <alan.bennett@linaro.org>
2017-08-14 21:34:05 -0600

    changes

--- Time Zones_files/data.js
+++ Time Zones_files/data.js
@@ -1,1 +1,1 @@
-var data = [[-7.0,"Mike, Tyler, Robert","PDT UTC-7",true],[-6.0,"Alan","MDT UTC-6",true],[-5.0,"Andy","CDT UTC-5",true],[-4.0,"Marti","EDT UTC-4",true],[-3.0,"Ricardo","BRT UTC-3",true],[0.0,"George, Yang","GMT UTC",true],[1.0,"Milo","CET UTC+1",true],[5.5, "Amit","IST UTC+5.5",true]];
+var data = [[-7.0,"Mike, Tyler","PDT UTC-7",true],[-6.0,"Alan","MDT UTC-6",true],[-5.0,"Andy","CDT UTC-5",true],[-4.0,"Marti","EDT UTC-4",true],[-3.0,"Ricardo","BRT UTC-3",true],[0.0,"George","GMT UTC",true],[1.0,"Milo","CET UTC+1",true]];
